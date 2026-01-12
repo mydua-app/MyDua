8acb6b0ece5cb38ad2a0a2ff37144cb4f5e0f2ce
mydua-app <mydua571@gmail.com>
2026-01-12 22:07:58 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,9 @@
 function toggleMenu() {
     const menu = document.getElementById("sideMenu");
-    menu.classList.toggle("open");
+    menu.style.left = menu.style.left === "0px" ? "-220px" : "0px";
+}
+
+function toggleProfileMenu() {
+    const menu = document.getElementById("profileMenu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
